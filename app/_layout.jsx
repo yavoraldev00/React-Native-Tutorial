496637ed553ad088af1a907d36cdd950add112c2
YAVOR ALDEV <y.aldev@student.fontys.nl>
2026-04-09 20:16:31 +0200

    Lesson 5 - Light and Dark Themes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,15 @@
 import { Slot, Stack } from 'expo-router'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
+import { Colors } from '../constants/Color'
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
+
   return (
     <Stack screenOptions={{
-        headerStyle: { backgroundColor: "#ddd" },
-        headerTintColor: "#333"
+        headerStyle: { backgroundColor: theme.navBackground },
+        headerTintColor: theme.title
     }}>
         <Stack.Screen name="index" options={{title: "Home"}} />
         <Stack.Screen name="about" options={{title: "About"}} />
--- app/about.jsx
+++ app/about.jsx
@@ -1,9 +1,13 @@
 import { Link } from 'expo-router'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
+import { Colors } from '../constants/Color'
 
 const About = () => {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor: theme.background}]}>
       <Text style={styles.title}>About Page</Text>
 
       <Link href="/" style={styles.link}>Back Home</Link>
